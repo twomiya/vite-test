e564278722e36c43417a88cd2e715848efbcf393
zuoxf <zuoxf@cyberpeace.cn>
2021-09-15 18:06:57 +0800

    feat: update

--- server/index.js
+++ server/index.js
@@ -1,20 +1,35 @@
 const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = 6060;
 
 var bodyParser = require("body-parser"); /*post方法*/
 app.use(bodyParser.json()); // 添加json解析
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(cors());
 //设置允许跨域访问该服务.
 app.all("*", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  //Access-Control-Allow-Headers ,可根据浏览器的F12查看,把对应的粘贴在这里就行
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", " 3.2.1");
   res.header("Content-Type", "application/json;charset=utf-8");
   next();
 });
+
+// 生成token
+function generateToken() {
+  return jwt.sign(
+    {
+      foo: "bar",
+    },
+    "hahaha",
+    {
+      expiresIn: "1d", // 1天 https://github.com/zeit/ms
+    }
+  );
+}
 
 app.get("/api/list", (req, res) =>
   res.json({
@@ -45,53 +60,15 @@ app.get("/api/list", (req, res) =>
   })
 );
 app.post("/login", (req, res) => {
-  console.log(req);
-  res.json({ data: req.body });
-});
-
-app.post("/interface/batch_info_mask", (req, res) => {
-  console.log(Date.now());
-  console.log(req.body);
   res.json({
-    code: 0,
-    result: [
-      {
-        user_id: 4155112,
-      },
-    ],
+    status: true,
+    data: {
+      token: generateToken(),
+    },
+    message: "登录成功！",
   });
+  // console.log(req);
+  // res.json({ data: req.body });
 });
-
-app.get("/log", (req, res) =>
-  res.json([
-    {
-      sign: "测试",
-      name: "包名",
-      cid: "测试渠道",
-      phone: "18888888888",
-      content: "噔噔噔噔噔噔噔噔噔噔",
-      success: false,
-      ts: 123123123,
-    },
-    {
-      sign: "测试",
-      name: "包名",
-      cid: "测试渠道",
-      phone: "18888888888",
-      content: "噔噔噔噔噔噔噔噔噔噔",
-      success: false,
-      ts: 158762391932111114,
-    },
-    {
-      sign: "测试",
-      name: "包名",
-      cid: "测试渠道",
-      phone: "18888888888",
-      content: "噔噔噔噔噔噔噔噔噔噔",
-      success: false,
-      ts: 1587623919321324,
-    },
-  ])
-);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
